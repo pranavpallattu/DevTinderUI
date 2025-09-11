bea7d81a1d2295d891cc3513dd74aa541503d228
pranavpallattu <pranavpspallattu@gmail.com>
2025-09-11 14:50:43 +0530

    login and redux setup

--- README.md
+++ README.md
@@ -10,8 +10,17 @@
 - Install react router dom
 - Create BrowserRouter > Routes > Route=/ Body > RouteChildren
 - Create an Outlet in your body component
-- 
-
+- Create a Footer
+- Create a Login page 
+- Install axios
+- CORS => Install cors => add cors middleware to app with configurations origin and credentials:true 
+- whenever you're making an api call with axios make {withcredentials:true}
+- Install Redux toolkit and read docs
+- Install redux toolkit and react-redux => configureStore => Provider => createSlice => add reducer to store
+- Add redux devtools in chrome
+- Login and see if your data is coming properly in store
+- Navbar should update as soon as user logs in 
+- Refactor our code to add constants file and components folder
 
 
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,59 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice'
+import { useNavigate } from 'react-router-dom'
+import { BASE_URL } from '../utils/constants'
+
+const Login = () => {
+
+  const[emailId,setEmailId]=useState("pranav@gmail.com")
+  const[password,setPassword]=useState("Pranav@123")
+
+  const dispatch=useDispatch()
+    const navigate=useNavigate()
+
+  const handleLogin= async()=>{
+    try{
+       const res=await axios.post( BASE_URL + "/login" ,{
+          emailId,
+          password
+        },{withCredentials:true})
+        console.log(res);
+        dispatch(addUser(res.data))
+        navigate("/")
+        
+    }
+    catch(error){
+      console.log(error);
+      
+    }
+  }
+
+
+
+  return (
+    <div className='flex justify-center my-32'>
+      <div className="card bg-base-300 text-neutral-content w-96">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title text-2xl">Login</h2>
+    <div className='w-full py-3'>
+      <fieldset className="fieldset py-4">
+  <input type="text" className="input py-3" placeholder="Email Id" value={emailId} onChange={(e)=>setEmailId(e.target.value)} />
+  {emailId}
+</fieldset>
+   <fieldset className="fieldset py-4">
+  <input type="text" className="input py-3" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+  {password}
+</fieldset>
+    </div>
+    <div className="card-actions justify-end">
+      <button onClick={handleLogin} className="btn btn-primary">Login</button>
+    </div>
+  </div>
+</div> 
+    </div>
+  )
+}
+
+export default Login
